697c85ea2a222928e074bc138179c325244675a6
fjavierlh <fjavierlh@gmail.com>
2021-05-03 17:29:12 +0200

    Imported App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./App";
 
 ReactDOM.render(
-    <div>Hola mundo</div>,
+    <App/>,
     document.getElementById("root")
 );
